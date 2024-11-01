4cfa7b09d31699fe3f5ae1fb62451b76195729de
Jens Schauder <jens@schauderhaft.de>
2024-11-01 15:47:27 +0100

    added actual test

--- src/test/java/de/schauderhaft/roadtoai/trivial/DemoApplicationTests.java
+++ src/test/java/de/schauderhaft/roadtoai/trivial/DemoApplicationTests.java
@@ -1,13 +1,27 @@
 package de.schauderhaft.roadtoai.trivial;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	ChatModel chatModel;
+
 	@Test
 	void contextLoads() {
+
+		ChatClient chatClient = ChatClient.create(chatModel);
+
+		String result = chatClient.prompt().user("Hi, who am I talking to?").call().content();
+
+		assertThat(result).isNotEmpty();
 	}
 
 }

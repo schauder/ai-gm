b8ea74f192bcaeb38a4580182b0212d9dea2650c
Jens Schauder <jens@schauderhaft.de>
2024-11-08 15:50:53 +0100

    A little Hello AI test does actually work.

--- src/test/java/de/schauderhaft/roadtoai/trivial/DemoApplicationTests.java
+++ src/test/java/de/schauderhaft/roadtoai/trivial/DemoApplicationTests.java
@@ -1,6 +1,8 @@
 package de.schauderhaft.roadtoai.trivial;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,17 +13,22 @@ import static org.assertj.core.api.Assertions.*;
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Autowired
-	ChatModel chatModel;
+	private static final Logger log = LoggerFactory.getLogger(DemoApplicationTests.class);
+
+	private final ChatClient chatClient;
+
+	DemoApplicationTests(@Autowired ChatModel chatModel) {
+		chatClient = ChatClient.create(chatModel);
+	}
 
 	@Test
-	void contextLoads() {
-
-		ChatClient chatClient = ChatClient.create(chatModel);
+	void helloOpenAi() {
 
 		String result = chatClient.prompt().user("Hi, who am I talking to?").call().content();
 
 		assertThat(result).isNotEmpty();
+
+		log.info(result);
 	}
 
 }
